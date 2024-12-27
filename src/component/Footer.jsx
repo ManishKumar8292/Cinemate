a72908a5399a5aeb3e57051941a55fae72dc344d
manishkumar8292 <manishverma9122@gmail.com>
2024-12-27 20:56:08 +0530

    issue fixed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -11,13 +11,13 @@ export const Footer = () => {
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-100 sm:mt-0">
                     <li>
-                        <a href="https://www.instagram.com/manishv1_2" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+                        <a href="https://www.instagram.com/manishv1_2" target="_blank" rel="noreferrer" className="hover:underline me-4 md:me-6">Instagram</a>
                     </li>
                     <li>
-                        <a href="https://www.linkedin.com/in/manish-kumar-bb579a192" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+                        <a href="https://www.linkedin.com/in/manish-kumar-bb579a192" target="_blank" rel="noreferrer" className="hover:underline me-4 md:me-6">LinkedIn</a>
                     </li>
                     <li>
-                        <a href="https://github.com/ManishKumar8292" target="_blank" className="hover:underline">Github</a>
+                        <a href="https://github.com/ManishKumar8292" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
                     </li>
                 </ul>
             </div>
